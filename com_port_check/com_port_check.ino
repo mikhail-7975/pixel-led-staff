5b7712d45cbc22c518c3df9084475105c8082c78
mikhail-7975 <mikhail.kalinichenko.1999@gmail.com>
2023-01-15 04:47:12 +0700

    add firmware file

---
+++ com_port_check/com_port_check.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  Serial.println("Hello!");
+  // put your main code here, to run repeatedly:
+
+}
